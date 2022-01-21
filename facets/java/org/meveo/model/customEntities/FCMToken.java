d0641dfa4110552fb6f495fd8d991a1a16958805
meveo.admin <no-mail@meveo.com>
2022-01-21 01:00:36 +0000

    Add the cet java source file : FCMToken.javain the module : firebase-messaging

---
+++ facets/java/org/meveo/model/customEntities/FCMToken.java
@@ -0,0 +1,63 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class FCMToken implements CustomEntity {
+
+    public FCMToken() {
+    }
+
+    public FCMToken(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String userId;
+
+    private String token;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "FCMToken";
+    }
+}
