392c8d0fe66d1cf07f61b9c2f6634d0d0d66b664
tonysviews <tonysviews@gmail.com>
2022-08-23 08:30:48 +0000

    Add the cet java source file : Notification.javain the module : mv-firebase-messaging

--- facets/java/org/meveo/model/customEntities/Notification.java
+++ facets/java/org/meveo/model/customEntities/Notification.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Notification implements CustomEntity {
@@ -20,18 +19,6 @@ public class Notification implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Instant sendDate;
-
-    private String recipient;
-
-    private String sendStatus;
-
-    private String notificationContents;
-
-    private String error;
-
-    private String title;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -47,54 +34,6 @@ public class Notification implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Instant getSendDate() {
-        return sendDate;
-    }
-
-    public void setSendDate(Instant sendDate) {
-        this.sendDate = sendDate;
-    }
-
-    public String getRecipient() {
-        return recipient;
-    }
-
-    public void setRecipient(String recipient) {
-        this.recipient = recipient;
-    }
-
-    public String getSendStatus() {
-        return sendStatus;
-    }
-
-    public void setSendStatus(String sendStatus) {
-        this.sendStatus = sendStatus;
-    }
-
-    public String getNotificationContents() {
-        return notificationContents;
-    }
-
-    public void setNotificationContents(String notificationContents) {
-        this.notificationContents = notificationContents;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     @Override()
